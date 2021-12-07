a2a46729dc293c6e4be0847f7a5cf8a57ca4b9b0
kek-Sec <georgepetrakis99@gmail.com>
2021-12-07 13:56:18 +0200

    Add project files.

---
+++ efTest/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using efTest.Data;
+using efTest.Models;
+
+namespace efTest.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly efTest.Data.SchoolContext _context;
+
+        public IndexModel(efTest.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students.ToListAsync();
+        }
+    }
+}
